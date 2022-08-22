09a87e87b5e5f3bd86e76d10b09225b9de85fade
Tingfeng <wu.tingfeng@u.nus.edu>
2022-08-22 21:25:04 +0800

    database requests in parallel

--- pages/index.js
+++ pages/index.js
@@ -71,47 +71,54 @@ export default function Home() {
   useEffect(() => {
     const fetchRepoMetaData = async () => {
       setIsLoading(true);
-      const archOfficial = await supabase
-        .from("arch_official")
-        .select("pkgname,repo,arch,pkgdesc")
-        .textSearch("pkgname", inputText)
-        .order("pkgname", { ascending: true })
-        .order("repo", { ascending: true })
-        .order("arch", { ascending: true })
-        .then(({ data, error }) => {
-          if (!error) {
-            return data.map((e) => {
-              return {
-                pkgname: e["pkgname"],
-                repo: e["repo"],
-                arch: e["arch"],
-                description: e["pkgdesc"],
-                isAUR: false,
-              };
-            });
-          }
-          return [];
-        });
-      const aur = await supabase
-        .from("aur")
-        .select("ID,Name,Description")
-        .textSearch("Name", inputText)
-        .order("Name", { ascending: true })
-        .order("ID", { ascending: true })
-        .then(({ data, error }) => {
-          if (!error) {
-            return data.map((e) => {
-              return {
-                pkgname: e["Name"],
-                ID: e["ID"],
-                description: e["Description"],
-                isAUR: true,
-              };
-            });
-          }
-          return [];
-        });
-      const data = archOfficial.concat(aur);
+      const archOfficial = async () =>
+        supabase
+          .from("arch_official")
+          .select("pkgname,repo,arch,pkgdesc")
+          .textSearch("pkgname", inputText)
+          .order("pkgname", { ascending: true })
+          .order("repo", { ascending: true })
+          .order("arch", { ascending: true })
+          .then(({ data, error }) => {
+            if (!error) {
+              return data.map((e) => {
+                return {
+                  pkgname: e["pkgname"],
+                  repo: e["repo"],
+                  arch: e["arch"],
+                  description: e["pkgdesc"],
+                  isAUR: false,
+                };
+              });
+            }
+            return [];
+          });
+      const aur = async () =>
+        supabase
+          .from("aur")
+          .select("ID,Name,Description")
+          .textSearch("Name", inputText)
+          .order("Name", { ascending: true })
+          .order("ID", { ascending: true })
+          .then(({ data, error }) => {
+            if (!error) {
+              return data.map((e) => {
+                return {
+                  pkgname: e["Name"],
+                  ID: e["ID"],
+                  description: e["Description"],
+                  isAUR: true,
+                };
+              });
+            }
+            return [];
+          });
+      const data = [].concat.apply(
+        [],
+        (await Promise.allSettled([archOfficial(), aur()]))
+          .filter((e) => e.status === "fulfilled")
+          .map((e) => e.value)
+      );
       setPackages(data);
       setIsLoading(false);
     };
